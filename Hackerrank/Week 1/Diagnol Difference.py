891a83b5f55ddf556467c5eacd792e76780d68eb
Aravindhan M <manoaravindh2003@gmail.com>
2025-08-08 10:37:06 +0530

    Week 1

---
+++ Hackerrank/Week 1/Diagnol Difference.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    primary_diagnol = 0
+    secondary_diagnol = 0
+    n = len(arr)
+    
+    for i in range(n):
+        primary_diagnol += arr[i][i]
+        secondary_diagnol += arr[i][n - 1 - i]
+    
+    return abs(primary_diagnol - secondary_diagnol)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
