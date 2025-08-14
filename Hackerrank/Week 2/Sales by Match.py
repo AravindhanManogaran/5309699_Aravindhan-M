b5e655c0cf7354dd63de8dbf9fbdb4be90cca4e9
Aravindhan M <manoaravindh2003@gmail.com>
2025-08-14 22:27:02 +0530

    Week 2

---
+++ Hackerrank/Week 2/Sales by Match.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    pairs = 0
+    sock_count = {}
+    for sock in ar:
+        sock_count[sock] = sock_count.get(sock, 0) + 1
+    for count in sock_count.values():
+        pairs += count // 2
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
